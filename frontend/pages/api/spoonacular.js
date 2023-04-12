7b3277c8b9d71e389f7985c2b41b05f346c4711a
Ishaan Khurana <63468587+ik04@users.noreply.github.com>
2023-04-12 15:32:19 +0530

    Update spoonacular.js

--- frontend/pages/api/spoonacular.js
+++ frontend/pages/api/spoonacular.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const apiKey = "b003a8d1459c44c7bccd48004c257852";
+  const apiKey = "YOUR-KEY";
   const { query } = req.query;
 
   const response = await axios.get(
